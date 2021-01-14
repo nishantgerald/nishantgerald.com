e5077270ef5c51cf418bbba46e66d8eb1bdf5be2
nishantgerald <nishant.gerald@gmail.com>
2021-01-13 21:56:18 -0500

    added header file

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Adding Google Analytics Global Site Tag-->
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179118177-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-179118177-1');
+  </script>
+  <!-- NAV BAR -->
+  <div class="topnav">
+    <a class="active" href="/">Home</a>
+    <a href="#intro">Intro</a>
+    <a href="#contact">Contact</a>
+    <a href="#work">Work</a>
+    <a href="/videos/index.php">Videos</a>
+  </div>
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  <link href="/assets/images/favicon.ico" rel="icon">
+  <link rel="apple-touch-icon" href="/assets/images/apple-touch-icon.png"/>
+  <title>Nishant Gerald</title>  
+
+  <link href="/assets/css/main.d8e0d294.css" rel="stylesheet">
+</head>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function (event) {
+     scrollRevelation('.card');
+  });
+</script>
+
+<!-- Google Analytics -->
+
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-179118177-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<script type="text/javascript" src="/assets/js/main.bc58148c.js"></script>
+
+</html>
